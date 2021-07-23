ba1e5168204ec55e71afc211246c40a4ccda1346
jami-shahab <sjami@my.yorku.ca>
2021-07-23 15:00:24 -0400

    Created mls class for handling mls.txt

---
+++ Utilities/MultipleListingService.java
@@ -0,0 +1,22 @@
+package Utilities;
+
+import Model.Land;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class MultipleListingService {
+    public static void createMlsRecord(Land property) throws IOException {
+        try {
+            FileWriter mls = new FileWriter("data/mls.txt");
+            mls.write(property.getMlsEntry() + "\n");
+            mls.close(); // close file when done writing to it
+        } catch (IOException err) {
+            System.out.println("Error occurred while writing to mls.txt");
+            err.printStackTrace(); // useful for finding problems in case of error
+        }
+    }
+
+
+}
