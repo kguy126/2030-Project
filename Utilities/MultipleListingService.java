3481116a921654c14e498610abe47ad2bd1c61c2
jami-shahab <sjami@my.yorku.ca>
2021-07-23 19:24:19 -0400

    Created structure for mls class. also removed redundant Address file

--- Utilities/MultipleListingService.java
+++ Utilities/MultipleListingService.java
@@ -1,22 +1,54 @@
 package Utilities;
 
+import Console.Main;
 import Model.Land;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class MultipleListingService {
-    public static void createMlsRecord(Land property) throws IOException {
-        try {
-            FileWriter mls = new FileWriter("data/mls.txt",true); //
-            mls.write(property.getMlsEntry() + "\n");
-            mls.close(); // close file when done writing to it
-        } catch (IOException err) {
-            System.out.println("Error occurred while writing to mls.txt");
-            err.printStackTrace(); // useful for finding problems in case of error
-        }
+    private static Map<UUID, Land> storage = new HashMap<UUID, Land>();
+
+    public static void addNewProperty(Land property) {
+        storage.put(property.getUuid(), property);
     }
 
+    public static Land searchById(UUID uuid) {
+        return storage.getOrDefault(uuid, null);
+    }
 
+    public static ArrayList<Land> searchBySize(double size) {
+        ArrayList<Land> result = new ArrayList<Land>();
+        for (UUID key : storage.keySet())
+            if (storage.get(key).getSizeInSquareMeters() >= size) result.add(storage.get(key));
+        if (result.size() == 0) return null;
+        return result;
+    }
+
+    public static ArrayList<Land> searchByCountry(String country) {
+        ArrayList<Land> result = new ArrayList<Land>();
+        for (UUID key: storage.keySet()) {
+            if (storage.get(key).getAddressObj().getCountryName().equals(country))
+                result.add(storage.get(key));
+        } if (result.size() == 0) return null;
+        return result;
+    }
+
+    public static ArrayList<Land> searchByProvinceOrState(String provinceOrState) {
+        ArrayList<Land> result = new ArrayList<Land>();
+        for (UUID key: storage.keySet()) {
+            if (storage.get(key).getAddressObj().getStateOrProvinceName().equals(provinceOrState))
+                result.add(storage.get(key));
+        } if (result.size() == 0) return null;
+        return result;
+    }
+
+    public static ArrayList<Land> searchByCity(String city) {
+        ArrayList<Land> result = new ArrayList<Land>();
+        for (UUID key: storage.keySet()) {
+            if (storage.get(key).getAddressObj().getCityName().equals(city))
+                result.add(storage.get(key));
+        } if (result.size() == 0) return null;
+        return result;
+    }
 }
