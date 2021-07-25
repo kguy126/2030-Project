cb45f638e9c76b139f0aad85d3a00ab53f9d3892
jami-shahab <sjami@my.yorku.ca>
2021-07-24 20:40:59 -0400

    fixed bugs

--- Model/LockersAndStorage.java
+++ Model/LockersAndStorage.java
@@ -4,92 +4,114 @@ import Utilities.Address;
 
 import java.util.UUID;
 
-public class LockersAndStorage {
-    private int size, floorNumber;
-    private Address address;
+public class LockersAndStorage extends Land {
+    // attributes pertaining to lockers and storage properties
+    private int floorCount;
+    private boolean isHeated, isNewConstruction;
+    private double rentalCost;
 
-    public LockersAndStorage(UUID uuid) {
-        this.setUuid(uuid);
-    }
-    
-    public void setSize(int size) {
-        this.size = size;
+    public LockersAndStorage (UUID uuid) { this.setUuid(uuid);}
+
+
+    public void setFloorCount(int floorCount) {
+        this.floorCount = floorCount;
     }
 
-    public int getSize() {
-        return size;
+    public int getFloorCount() {
+        return floorCount;
     }
-    
-    
+
+    public boolean isHeated() {
+        return isHeated;
+    }
+
+    public void setHeated(boolean heated) {
+        isHeated = heated;
+    }
+
+    public double getRentalCost() {
+        return rentalCost;
+    }
+
+    public void setRentalCost(double rentalCost) {
+        this.rentalCost = rentalCost;
+    }
+
+    public boolean isNewConstruction() {
+        return isNewConstruction;
+    }
+
+    public void setNewConstruction(boolean newConstruction) {
+        isNewConstruction = newConstruction;
+    }
+
     @Override
-    public int getFloorNumber() {
-        return floorNumber;
+    public String toString() {
+        return "LockersAndStorage{" +
+                super.toString() +
+                ", floorCount=" + floorCount +
+                ", isHeated=" + isHeated +
+                ", rentalCost=" + rentalCost +
+                '}';
     }
 
-    @Override
-    public void setFloorNumber(int floorNumber) {
-        this.floorNumber = floorNumber;
-    
 
-    @Override
-    public void updateMlsEntry() {
-        super.updateMlsEntry();
-        String toAdd = "";
-        toAdd += ", Property Type: Lockers and Storage";
-        toAdd += ", Size (square footage): " + this.size;
-        toAdd += ", Located at floor level: " + this.floorNumber;
-        this.addToMlsEntry(toAdd + " }");
-    }
+
 
     public static class Builder {
+        // required attribtues for instantiation
         private UUID uuid;
         private Address address;
-        int size, floorNumber;
-        double price;
-        boolean isNewConstruction;
-        boolean isHeated;
+        private double sizeInSquareMeters, rentalCost;
+        private boolean isNewConstruction, isHeated;
+        private int floorCount;
 
         public Builder(UUID uuid) {
             this.uuid = uuid;
         }
 
+        /**
+         * copy made so reference is not shared by this method which sets the address
+         *
+         * @param address Address object for setting the address
+         * @return the Builder class itself for chaining and easier instantiation
+         */
         public Builder locatedAt(Address address) {
-            this.address = address;
+            this.address = new Address(address);
             return this;
         }
 
-        public Builder pricedAt(double price) {
-            this.price = price;
+        public Builder costing(double rentalCost) {
+            this.rentalCost = rentalCost;
             return this;
         }
 
-        public Builder sizeAt(int size) {
-            this.size = size;
+        public Builder withSizeInSquareMeters(double sizeInSquareMeters) {
+            this.sizeInSquareMeters = sizeInSquareMeters;
             return this;
         }
 
-        public Builder setAsNewConstruct(boolean isNewConstruction) {
-            this.isNewConstruction = isNewConstruction;
-            return this;
-        }
-        
-         public Builder setAsHeated(boolean isHeated) {
+        public Builder setIsHeated(boolean isHeated) {
             this.isHeated = isHeated;
             return this;
         }
 
-        public Builder locatedOnFloorNum(int floorNumber) {
-            this.floorNumber = floorNumber;
+        public Builder floorsCountedAt(int floorCount) {
+            this.floorCount = floorCount;
             return this;
         }
-        
+
         public LockersAndStorage build() {
-            parkingSpace parkingSpace = new ParkingSpace(this.uuid);
-            parkingSpace.setAddress(this.address);
-            parkingSpace.setPrice(this.price);
-            parkingSpace.setSize(this.size);
-            parkingSpace.setFloorNumber(this.floorNumber);
+            LockersAndStorage lockersAndStorage = new LockersAndStorage(uuid);
+            lockersAndStorage.setAddress(address);
+            lockersAndStorage.setSizeInSquareMeters(sizeInSquareMeters);
+            lockersAndStorage.setHeated(isHeated);
+            lockersAndStorage.setFloorCount(floorCount);
+            lockersAndStorage.setNewConstruction(isNewConstruction);
+            lockersAndStorage.setRentalCost(rentalCost);
             return lockersAndStorage;
         }
     }
+
+
 }
