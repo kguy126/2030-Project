1f05f043869a3fd5740a374b3439b38a08297539
jami-shahab <sjami@my.yorku.ca>
2021-07-24 20:43:53 -0400

    fixed bugs

--- Model/LockersAndStorage.java
+++ Model/LockersAndStorage.java
@@ -4,7 +4,6 @@ import Utilities.Address;
 
 import java.util.UUID;
 
-<<<<<<< HEAD
 public class LockersAndStorage extends Land {
     // attributes pertaining to lockers and storage properties
     private int floorCount;
@@ -45,49 +44,11 @@ public class LockersAndStorage extends Land {
     public void setNewConstruction(boolean newConstruction) {
         isNewConstruction = newConstruction;
     }
-=======
-public class LockersAndStorage {
-    // attributes unique to a multiplex property
-    private int floorNumber;
-    
-    /**
-     * constructor to set the uuid of the property
-     *
-     * @param uuid identification for the property
-     */
-    public LockersAndStorage(UUID uuid) {
-        this.setUuid(uuid);
-    }
-   
-    /**
-     * method for setting the floor number/level the storage is located at
-     *
-     * @param floorNumber integer representing the floor number
-     */
-    public void setFloorNumber(int floorNumber) {
-        this.floorNumber = floorNumber;
-        
-    /**
-     * method for getting the floor number/level the storage is located at
-     *
-     * @return integer representing the floor number
-     */        
-    public int getFloorNumber() {
-        return floorNumber;
-    }
-    
->>>>>>> b105e78ec6053c63e73fd1fd2e102dcb75cdd572
 
-     /**
-     * method to continue representing instantiated property's attributes as a String
-     *
-     * @return string representation of attributes
-     */
     @Override
     public String toString() {
         return "LockersAndStorage{" +
                 super.toString() +
-<<<<<<< HEAD
                 ", floorCount=" + floorCount +
                 ", isHeated=" + isHeated +
                 ", rentalCost=" + rentalCost +
@@ -111,37 +72,6 @@ public class LockersAndStorage {
 
         /**
          * copy made so reference is not shared by this method which sets the address
-=======
-                ", floorNumber=" + floorNumber +
-                '}';
-    }
-
-    /**
-     * this class is meant to be instantiated (as its not an abstract)
-     * a builder design patterns with all the required attributes is therefore implemented
-     * this allows for more simple instantiation for the client
-     */
-    public static class Builder {
-        // required attributes for instantiation
-        private UUID uuid;
-        private Address address;
-        int size, floorNumber;
-        double price;
-        boolean isNewConstruction;
-        boolean isHeated;
-        
-        /**
-         * constructor of Builder class to start with implementing the uuid
-         *
-         * @param uuid identification for the property
-         */
-        public Builder(UUID uuid) {
-            this.uuid = uuid;
-        }
-        
-        /**
-         * copy made so refrence is not shared by this method which sets the address
->>>>>>> b105e78ec6053c63e73fd1fd2e102dcb75cdd572
          *
          * @param address Address object for setting the address
          * @return the Builder class itself for chaining and easier instantiation
@@ -150,7 +80,6 @@ public class LockersAndStorage {
             this.address = new Address(address);
             return this;
         }
-<<<<<<< HEAD
 
         public Builder costing(double rentalCost) {
             this.rentalCost = rentalCost;
@@ -163,53 +92,10 @@ public class LockersAndStorage {
         }
 
         public Builder setIsHeated(boolean isHeated) {
-=======
-        
-        /**
-         * method for setting price
-         *
-         * @param price double representing price of property
-         * @return the Builder class itself for chaining and easier instantiation
-         */
-        public Builder pricedAt(double price) {
-            this.price = price;
-            return this;
-        }
-        
-        /**
-         * method for setting the size of the property in square meters
-         *
-         * @param size double representing size of property
-         * @return the Builder class itself for chaining and easier instantiation
-         */
-        public Builder withSizeInSquareMeters(double size) {
-            this.sizeInSquareMeters = size;
-            return this;
-           
-        /**
-         * method for setting whether or not the property is new
-         *
-         * @param isNewConstruction boolean variable representing if this property is newly constructed
-         * @return the Builder class itself for chaining and easier instantiation
-         */
-        public Builder setAsNewConstruct(boolean isNewConstruction) {
-            this.isNewConstruction = isNewConstruction;
-            return this;
-        }
-
-        /**
-         * method for setting whether or not the property is heated
-         *
-         * @param isHeated boolean variable representing if this property is heated
-         * @return the Builder class itself for chaining and easier instantiation
-         */
-         public Builder setAsHeated(boolean isHeated) {
->>>>>>> b105e78ec6053c63e73fd1fd2e102dcb75cdd572
             this.isHeated = isHeated;
             return this;
         }
 
-<<<<<<< HEAD
         public Builder floorsCountedAt(int floorCount) {
             this.floorCount = floorCount;
             return this;
@@ -228,34 +114,4 @@ public class LockersAndStorage {
     }
 
 
-=======
-        /**
-         * method for setting the floorNumber
-         *
-         * @param floorNumber integer representing the floor (level) which the locker/storage is located
-         * @return the Builder class itself for chaining and easier instantiation
-         */
-        public Builder locatedOnFloorNum(int floorNumber) {
-            this.floorNumber = floorNumber;
-            return this;
-        }
-        
-        /**
-         * build method to be called after setting all attributes
-         * will create new ParkingSpace object and set its attributes
-         * attributes are set based on that which are set in the Builder class
-         * the object with set attributes is then returned
-         *
-         * @return an object of ParkingSpace class
-         */
-        public LockersAndStorage build() {
-            lockersAndStorage lockersAndStorage = new LockersAndStorage(this.uuid);
-            lockersAndStorage.setAddress(this.address);
-            lockersAndStorage.setPrice(this.price);
-            lockersAndStorage.ssetSizeInSquareMeters(this.sizeInSquareMeters);
-            lockersAndStorage.setFloorNumber(this.floorNumber);
-            return lockersAndStorage;
-        }
-    }
->>>>>>> b105e78ec6053c63e73fd1fd2e102dcb75cdd572
 }
